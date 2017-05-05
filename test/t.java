9ae3b3c507de3c5e7a3bd83d5c8385e9f5e15f76
welleswangli <9871492@qq.com>
2017-05-05 14:51:25 +0800

    20170505

---
+++ test/t.java
@@ -0,0 +1,28 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.testng.TestNG;
+import org.testng.annotations.Test;
+
+public class t {
+	FirefoxDriver driver;
+	
+    public static void main( String[] args )
+    {
+        System.out.println((System.getProperty("user.dir")+"\\testng.xml"));
+        List<String> suites = new ArrayList<String>();
+        suites.add(System.getProperty("user.dir")+"\\testng.xml");
+        TestNG tng = new TestNG();
+        tng.setTestSuites(suites);
+        tng.run(); 
+    }
+    
+  @Test
+  public void f() {
+	  driver=new FirefoxDriver();
+	  driver.get("http://www.baidu.com");
+  }
+}
